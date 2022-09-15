fff500d3137b0ad502ca3a3b93b39bc43726dea4
vaux <zero.griffin@gmail.com>
2022-09-15 14:00:12 +0200

    Update card

--- iOS/Card.swift
+++ iOS/Card.swift
@@ -1,26 +1,25 @@
 import SwiftUI
 
 struct Card: ViewModifier {
-    var fill: Color?
-    private let base = RoundedRectangle(cornerRadius: 26, style: .continuous)
+    let fill: Color
+    private let base = RoundedRectangle(cornerRadius: 30, style: .continuous)
     
     func body(content: Content) -> some View {
         ZStack {
             base
                 .fill(Color(.tertiarySystemBackground))
             
-            if let fill {
-                base
-                    .fill(LinearGradient(colors: [fill, fill.opacity(0.6)],
-                                         startPoint: .topLeading,
-                                         endPoint: .bottomTrailing)
-                        .shadow(.inner(color: .white, radius: 1)))
-            }
+            base
+                .fill(LinearGradient(colors: [fill, fill.opacity(0.6)],
+                                     startPoint: .topLeading,
+                                     endPoint: .bottomTrailing)
+                    .shadow(.inner(color: .white, radius: 1)))
             
             content
                 .padding(18)
         }
-        .shadow(color: fill == nil ? .init(white: 0, opacity: 0.4) : fill!.opacity(0.4), radius: 4)
+        .foregroundColor(.white)
+        .shadow(color: fill.opacity(0.4), radius: 4)
         .padding(.horizontal)
     }
 }
--- iOS/Main.swift
+++ iOS/Main.swift
@@ -4,19 +4,47 @@ struct Main: View {
     var body: some View {
         ScrollView {
             VStack(spacing: 0) {
-                HStack(alignment: .top) {
+                VStack(spacing: 0) {
                     Text("78\(Text(verbatim: "%").font(.system(size: 20, weight: .regular)).baselineOffset(24).foregroundColor(.white.opacity(0.6)))")
                         .font(.system(size: 60, weight: .semibold).monospacedDigit())
-                        .foregroundColor(.white)
-                        .padding(.vertical, 60)
-                        .modifier(Card(fill: .blue))
-                        .padding(.top)
+                        .padding(.top, 20)
+                    Text("You are doing great!")
+                        .font(.callout)
+                        .foregroundStyle(.secondary)
+                    ZStack(alignment: .leading) {
+                        Capsule()
+                            .foregroundStyle(.tertiary)
+                        Capsule()
+                            .frame(width: 200)
+                    }
+                    .frame(height: 3)
+                    .padding(.vertical, 25)
+                    .padding(.horizontal, 20)
+                    
+                    HStack(alignment: .firstTextBaseline, spacing: 0) {
+                        Spacer()
+                        Spacer()
+                        Text("\(Text("Calories").font(.footnote).foregroundColor(.white.opacity(0.6)))\n300")
+                        Spacer()
+                        Text("\(Text("Distance").font(.footnote).foregroundColor(.white.opacity(0.6)))\n1,9km")
+                        Spacer()
+                        Text("\(Text("Steps").font(.footnote).foregroundColor(.white.opacity(0.6)))\n546")
+                        Spacer()
+                        Spacer()
+                    }
+                    .font(.title3.weight(.semibold).monospacedDigit())
+                    .multilineTextAlignment(.center)
+                    .padding(.bottom, 20)
                 }
+                .modifier(Card(fill: .blue))
+                .padding(.top, 20)
             }
-            .frame(maxWidth: .greatestFiniteMagnitude)
         }
         .background {
-            LinearGradient(colors: [.blue.opacity(0.4), .blue.opacity(0.1)], startPoint: .top, endPoint: .bottom)
+            LinearGradient(stops: [.init(color: .blue.opacity(0.6), location: 0),
+                                   .init(color: .blue.opacity(0.3), location: 0.4),
+                                   .init(color: .blue.opacity(0.2), location: 0.5),
+                                   .init(color: .blue.opacity(0.15), location: 1)], startPoint: .top, endPoint: .bottom)
                 .ignoresSafeArea(edges: .all)
         }
     }
