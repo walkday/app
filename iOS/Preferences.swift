1461665c54344fea8d2174b946e349a44fb2610e
vaux <zero.griffin@gmail.com>
2022-09-16 14:32:35 +0200

    Add review

--- iOS/Preferences.swift
+++ iOS/Preferences.swift
@@ -1,10 +1,17 @@
 import SwiftUI
+import StoreKit
 
 struct Preferences: View {
+    @Environment(\.requestReview) private var review
     
     var body: some View {
         VStack {
-            ShareLink("", item: URL(string: "https://www.hackingwithswift.com")!)
+            ShareLink("", item: URL(string: "https://www.swanspotting.com")!)
+            Button {
+                review()
+            } label: {
+                Text("Review")
+            }
             Spacer()
         }
     }
