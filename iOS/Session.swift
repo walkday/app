7994c42abbd0c8fb0aff22cc65089f72080ed2ca
vaux <zero.griffin@gmail.com>
2022-09-26 17:09:06 +0200

    Update session

--- iOS/Session.swift
+++ iOS/Session.swift
@@ -1,14 +1,46 @@
 import SwiftUI
 import Charts
+import CloudKit
 import Walker
+import Archivable
 
 final class Session: ObservableObject {
     @Published private(set) var walks = [Walk]()
     @Published private(set) var challenge: Challenge?
     let color: Color
+    let cloud = Cloud<Archive, CKContainer>.new(identifier: "iCloud.WalkDay")
     
     init() {
         color = [Color.blue, .purple, .indigo, .pink, .orange, .teal, .mint, .cyan].randomElement()!
+        
+        cloud
+            .combineLatest($provider
+                .compactMap {
+                    $0
+                }) { model, provider in
+                model.items(provider: provider)
+            }
+            .removeDuplicates()
+            .combineLatest($showing, $search) { items, showing, search in
+                items
+                    .filter { element in
+                        switch showing {
+                        case 0:
+                            return true
+                        case 1:
+                            return element.status == .new
+                        default:
+                            return element.status == .bookmarked
+                        }
+                    }
+                    .filter(search: search)
+                    .sorted()
+            }
+            .removeDuplicates()
+            .sink {
+                
+            }
+            .store(in: &Substring)
     }
     
     func find(location: CGPoint, overlay: ChartProxy, proxy: GeometryProxy) -> Walk? {
