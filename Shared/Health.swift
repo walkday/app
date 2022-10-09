0b5288e368573020d3179d712a456f79718c83f5
vaux <zero.griffin@gmail.com>
2022-10-09 18:40:07 +0200

    Update health

--- Shared/Health.swift
+++ Shared/Health.swift
@@ -26,58 +26,44 @@ final class Health {
                 for: Calendar.current.date(byAdding: .day, value: -13, to: .now) ?? .now),
             end: nil)
         
-        [Metric(identifier: .stepCount, unit: .count()) {
-            $0.steps = $1
-        },
-         .init(identifier: .distanceWalkingRunning, unit: .meter()) {
-             $0.distance = $1
-         },
-         .init(identifier: .activeEnergyBurned, unit: .largeCalorie()) {
-             $0.calories = $1
-         }]
-            .forEach { metric in
-                let query = HKStatisticsCollectionQuery(
-                    quantityType: .init(metric.identifier),
-                    quantitySamplePredicate: predicate,
-                    options: .cumulativeSum,
-                    anchorDate: Calendar.current.startOfDay(for: .now),
-                    intervalComponents: .init(day: 1))
-                
-                query.initialResultsHandler = { _, results, _ in
-                    guard let values = results?.values(unit: metric.unit) else { return }
-                    Task {
-                        await metric.add(values: values, read: read, write: write)
-                    }
+        [Metric(identifier: .stepCount, unit: .count(), keyPath: \.steps),
+         .init(identifier: .distanceWalkingRunning, unit: .meter(), keyPath: \.distance),
+         .init(identifier: .activeEnergyBurned, unit: .largeCalorie(), keyPath: \.calories)
+        ].forEach { metric in
+            let query = HKStatisticsCollectionQuery(
+                quantityType: .init(metric.identifier),
+                quantitySamplePredicate: predicate,
+                options: .cumulativeSum,
+                anchorDate: Calendar.current.startOfDay(for: .now),
+                intervalComponents: .init(day: 1))
+            
+            query.initialResultsHandler = { _, results, _ in
+                guard let values = results?.values(unit: metric.unit) else { return }
+                Task {
+                    await metric.add(values: values, read: read, write: write)
                 }
-
-                query.statisticsUpdateHandler = { _, _, results, _ in
-                    guard let values = results?.values(unit: metric.unit) else { return }
-                    Task {
-                        await metric.add(values: values, read: read, write: write)
-                    }
-                }
-
-                store.execute(query)
-                queries.insert(query)
             }
+            
+            query.statisticsUpdateHandler = { _, _, results, _ in
+                guard let values = results?.values(unit: metric.unit) else { return }
+                Task {
+                    await metric.add(values: values, read: read, write: write)
+                }
+            }
+            
+            store.execute(query)
+            queries.insert(query)
+        }
     }
     
     private struct Metric: @unchecked Sendable {
         let identifier: HKQuantityTypeIdentifier
         let unit: HKUnit
-        let update: @MainActor (inout Walk, Int) -> Void
+        let keyPath: WritableKeyPath<Walk, Int>
         
         @MainActor func add(values: [Date : Int], read: @escaping Health.Read, write: @escaping Health.Write) async {
             var walks = read()
-            
-            values
-                .forEach { item in
-                    walks
-                        .update(date: item.key) { walk in
-                            update(&walk, item.value)
-                        }
-                }
-            
+            walks.update(items: values, keyPath: keyPath)
             write(walks.sorted().suffix(14))
         }
     }
--- Walker/Sources/Array.Walk.swift
+++ Walker/Sources/Array.Walk.swift
@@ -1,13 +1,16 @@
 import Foundation
 
 extension Array where Element == Walk {
-    mutating public func update(date: Date, element: (inout Element) -> Void) {
-        if let index = firstIndex(where: { $0.date == date }) {
-            element(&self[index])
-        } else {
-            var item = Walk(date: date)
-            element(&item)
-            append(item)
-        }
+    mutating public func update(items: [Date : Int], keyPath: WritableKeyPath<Element, Int>) {
+        items
+            .forEach { item in
+                if let index = firstIndex(where: { $0.date == item.key }) {
+                    self[index][keyPath: keyPath] = item.value
+                } else {
+                    var walk = Walk(date: item.key)
+                    walk[keyPath: keyPath] = item.value
+                    append(walk)
+                }
+            }
     }
 }
