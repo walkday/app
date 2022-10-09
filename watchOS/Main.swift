6b4f8906beac37ca1a96b4cb38650793e166eac6
vaux <zero.griffin@gmail.com>
2022-10-09 17:07:29 +0200

    Add watch target

---
+++ watchOS/Main.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct Main: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
