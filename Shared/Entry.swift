e80307456a284aa5c99aee2a3c6bb9c135a16994
vaux <zero.griffin@gmail.com>
2022-10-24 18:07:30 -0500

    Remove tests

--- Shared/Entry.swift
+++ Shared/Entry.swift
@@ -2,13 +2,6 @@ import WidgetKit
 import Walker
 
 struct Entry: TimelineEntry {
-    #warning("test")
-    init(walk: Walk? = nil, challenge: Challenge = .init(), error: Error? = nil) {
-        self.walk = walk
-        self.challenge = challenge
-        self.error = error
-    }
-    
     let walk: Walk?
     let challenge: Challenge
     let error: Error?
